e42c1ab4c5cd5ff0f3e9412d0f3870880da009d4
Pc-Maison <rachid.rezig69@outlook.fr>
2022-11-30 18:35:32 +0100

    importation du script de creation de membre et adapation au support

--- JavaScript/Code.js
+++ JavaScript/Code.js
@@ -23,5 +23,66 @@ else{
 }
 
 
+function AdministrationMembre(){
+        var NumIdMembre = 0;
+                
+        class Membre {
+            constructor( ID, Nom, Prenom, AnnerNaissance, Portable, Majeur){ //      
+                this.ID = ID;//
+                this.Nom = Nom;//
+                this.Prenom = Prenom;  //                                             // Creation de l'objet Membre
+                this.AnnerNaissance = AnnerNaissance;//
+                this.Portable = Portable;//
+                this.Majeur = Majeur;//
+        
+            
+                
+            }
+    MontrerInformation() {   
+                    document.write ( "Id : " + this.ID + "<br> Nom : " + this.Nom + "<br> Prenom : " + this.Prenom + "<br> Portable : " + this.Portable + "<br> Majeur: " + this.Majeur ); 
+                    // creation de la fonction qui afficher les information d'un membre
+                }
+                        
+                }
+                
+                    AskCreationMembre = prompt("voulez vous crée un nouveau memrbe (Oui/Non) : ");
+
+                    if (AskCreationMembre = "Oui") {
+                    
+                        NumIdMembre += 1; // comptage pour l'id du membre
+                        var nom = prompt("Entrer le nom du membre"); //
+                        var prenom = prompt("Entrer le prenom du membre");//
+                        var annerNaissance = parseInt(prompt("Entrer l'anner de naissancce du membre"));   // bloc des saisies des informations
+                        var portable = prompt("Entrer le portable du membre");//
+                        var age = (2022 - annerNaissance);
+                        
+                        if( age >= 18) {
+                                majeur = "oui";             // verification de l'age
+                        }
+
+                        else {
+                                majeur = "non";
+                        }
+                
+                        const nouveau = new Membre(NumIdMembre ,nom, prenom, annerNaissance, portable, majeur);  //creation avec l'objet membre avec les information saisie
+                        nouveau.MontrerInformation();
+                        
+
+                    }
+
+                    else {
+                        
+                        if (AskCreationMembre === "Non") {                                             // creation de la fonction qui demande la creation d'un nouveau membre
+                        document.write("non");
+                        }
+                        else{
+                        document.write("Saisie incorecte");
+                        }
+                    }
+
+            let TableMembre = [Membre];    // creation d'un tableau qui stoque les instance de l'objet membre
+    }
+
+
 
 
