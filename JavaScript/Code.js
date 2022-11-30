b9f068b502258f93aeb98077f913f47b1a3655ea
Pc-Maison <rachid.rezig69@outlook.fr>
2022-11-30 21:51:49 +0100

    refont du programme et blockage sur un probelmme thenique

--- JavaScript/Code.js
+++ JavaScript/Code.js
@@ -23,66 +23,58 @@ else{
 }
 
 
-function AdministrationMembre(){
-        var NumIdMembre = 0;
-                
-        class Membre {
-            constructor( ID, Nom, Prenom, AnnerNaissance, Portable, Majeur){ //      
-                this.ID = ID;//
-                this.Nom = Nom;//
-                this.Prenom = Prenom;  //                                             // Creation de l'objet Membre
-                this.AnnerNaissance = AnnerNaissance;//
-                this.Portable = Portable;//
-                this.Majeur = Majeur;//
+let TableauMembre = new Array() // creation du tableau ou seron stocker les membres
+
+
+class Membre {
+    constructor( ID, Nom, Prenom, AnnerNaissance, Portable, Majeur){ //      
+        this.ID = ID;//
+        this.Nom = Nom;//
+        this.Prenom = Prenom;  //                                             // Creation de l'objet Membre
+        this.AnnerNaissance = AnnerNaissance;//
+        this.Portable = Portable;//
+        this.Majeur = Majeur;//
+    }
+}
+
+ 
+function CreationMembre(){
+
+
+    AskCreationMembre = prompt("voulez vous crée un nouveau memrbe (Oui/Non) : "); // demande de creation de mmembre
+
+    if (AskCreationMembre = "Oui") {
+    
+        NumIdMembre += 1; // comptage pour l'id du membre
+        var nom = prompt("Entrer le nom du membre"); // saisie du nom du membre
+        var prenom = prompt("Entrer le prenom du membre");//saisie du prenom du membre
+        var annerNaissance = parseInt(prompt("Entrer l'anner de naissancce du membre"));  //saisie de l'anner de naissance du membre
+        var portable = prompt("Entrer le portable du membre");//saisie du nemereau de telephone du mmembre
+        var age = (2022 - annerNaissance);//Calcul de l'age du mmembre
         
-            
-                
-            }
-    MontrerInformation() {   
-                    document.write ( "Id : " + this.ID + "<br> Nom : " + this.Nom + "<br> Prenom : " + this.Prenom + "<br> Portable : " + this.Portable + "<br> Majeur: " + this.Majeur ); 
-                    // creation de la fonction qui afficher les information d'un membre
-                }
-                        
-                }
-                
-                    AskCreationMembre = prompt("voulez vous crée un nouveau memrbe (Oui/Non) : ");
+        if( age >= 18) { // si membre majeur
+                majeur = "oui";  // alors la variable majeur prend la valeur oui            
+        }
 
-                    if (AskCreationMembre = "Oui") {
-                    
-                        NumIdMembre += 1; // comptage pour l'id du membre
-                        var nom = prompt("Entrer le nom du membre"); //
-                        var prenom = prompt("Entrer le prenom du membre");//
-                        var annerNaissance = parseInt(prompt("Entrer l'anner de naissancce du membre"));   // bloc des saisies des informations
-                        var portable = prompt("Entrer le portable du membre");//
-                        var age = (2022 - annerNaissance);
-                        
-                        if( age >= 18) {
-                                majeur = "oui";             // verification de l'age
-                        }
+        else { // sinon 
+                majeur = "non"; //la variable majeur prend la valeur non
+        }
 
-                        else {
-                                majeur = "non";
-                        }
-                
-                        const nouveau = new Membre(NumIdMembre ,nom, prenom, annerNaissance, portable, majeur);  //creation avec l'objet membre avec les information saisie
-                        nouveau.MontrerInformation();
-                        
-
-                    }
-
-                    else {
-                        
-                        if (AskCreationMembre === "Non") {                                             // creation de la fonction qui demande la creation d'un nouveau membre
-                        document.write("non");
-                        }
-                        else{
-                        document.write("Saisie incorecte");
-                        }
-                    }
-
-            let TableMembre = [Membre];    // creation d'un tableau qui stoque les instance de l'objet membre
     }
 
+    else {
+            
+            if (AskCreationMembre === "Non") {                                            
+                document.write("non");
+            }
+            
+            else{
+                document.write("Saisie incorecte");
+            }
+        }
+    
 
+    const nouveau = new Membre(NumIdMembre ,nom, prenom, annerNaissance, portable, majeur);  //creation avec l'objet membre avec les information saisie
 
+    }
 
