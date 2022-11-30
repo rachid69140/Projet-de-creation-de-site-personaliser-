fb08fed608abb1cebbc7b89f98715391f8b1f803
Pc-Maison <rachid.rezig69@outlook.fr>
2022-11-30 17:31:41 +0100

    creation des pages admin et creation du script de changement de pae lors de la coneion

--- JavaScript/Code.js
+++ JavaScript/Code.js
@@ -1,7 +1,25 @@
 
 
 function Test() {
-    alert("ca marche") ;
+var Utilisateur = prompt("Entrer le nom d'utilisateur : ")
+var Mdp = prompt("Entrer votre Mot de passe: ")
+if (Utilisateur ==="Admin" && Mdp === "Root"){
+    window.open("../Pages/PageAdmin/indexAdmin.html")
+    
+    
+
+}
+else if  (Utilisateur ==="Admin"){
+    alert("Mot de passe eronner")
+}
+else if  (Mdp ==="Root"){
+    alert("Utilisateur  eronner")
+}
+    
+else{
+    alert("Utilisateur et MDP  erroner")
+}
+   
 }
 
 
